3bc0e98ebd858fe163f5281ebb3adb7ed0089408
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-14 02:31:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @team_name = params[team_name]
+    @team_name = params[:team_name]
     @coach = params[:coach]
     @point_gaurd = params[:point_gaurd]
     @shooting_gaurd = params[:shooting_gaurd]
