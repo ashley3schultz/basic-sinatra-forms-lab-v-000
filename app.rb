3870359cf3fbd04943d7c1e75f2fd687dae4e25a
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-14 02:36:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
   post '/team' do
     @team_name = params[:team_name]
     @coach = params[:coach]
-    @point_gaurd = params[:point_gaurd]
-    @shooting_gaurd = params[:shooting_gaurd]
+    @point_guard = params[:point_guard]
+    @shooting_guard = params[:shooting_guard]
     @small_forward = params[:small_forward]
     @power_forward = params[:power_forward]
     @center = params[:center]
